429632c55395a4649b17a37c96487a0f35fc61bf
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-09-17 17:45:08 +0200

    Updated app with a shop page, collection preview component and colection item component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from 'react-router-dom'
 
-import HomePage from './pages/homepage/homepage.component.jsx';
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
-import './App.css';
+import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
