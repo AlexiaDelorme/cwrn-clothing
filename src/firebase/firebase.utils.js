8ed749dee433226ac73261ad31cd6e81c54c6186
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-09-23 19:36:46 +0200

    Added sign up form and authentication using firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,23 +3,48 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyDX5ejKIaaqLEJs1WfBStKVsYYoxntmjIg",
-    authDomain: "cwrn-db-ea037.firebaseapp.com",
-    databaseURL: "https://cwrn-db-ea037.firebaseio.com",
-    projectId: "cwrn-db-ea037",
-    storageBucket: "cwrn-db-ea037.appspot.com",
-    messagingSenderId: "456397293155",
-    appId: "1:456397293155:web:9ca24dd0c88dd6f6249a56",
-    measurementId: "G-N0TV06Q68M"
-}
+  apiKey: "AIzaSyDX5ejKIaaqLEJs1WfBStKVsYYoxntmjIg",
+  authDomain: "cwrn-db-ea037.firebaseapp.com",
+  databaseURL: "https://cwrn-db-ea037.firebaseio.com",
+  projectId: "cwrn-db-ea037",
+  storageBucket: "cwrn-db-ea037.appspot.com",
+  messagingSenderId: "456397293155",
+  appId: "1:456397293155:web:9ca24dd0c88dd6f6249a56",
+  measurementId: "G-N0TV06Q68M",
+};
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account'})
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
